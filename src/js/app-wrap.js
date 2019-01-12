7c461d2e2a09338063dd4aa8066dd26ee1cd4568
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-01-12 20:18:43 +0100

    Set background from storage on initialization

--- src/js/app-wrap.js
+++ src/js/app-wrap.js
@@ -4,17 +4,22 @@
  * TODO: remember open windows & positions
  */
 import './app-icons.js'
+import * as _ from './helpers.js'
+import Storage from './storage.js'
+const storage = new Storage()
 
 export default class AppWrap extends window.HTMLElement {
   constructor () {
     super()
     const template = document.createElement('template')
     template.innerHTML = /* html */ `
-      <h1 id="logo">Personal desktop</h1>
       <app-icons></app-icons>
     `
+
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.appendChild(template.content.cloneNode(true))
+    this.background = storage.get('settings-background')
+    if (this.background) { _.setBackgroundImage(this.background, document.body) }
   }
 }
 
