c408cf39ea1cd45d1d35877318922d582600b290
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-01-14 00:02:21 +0100

    Use `section` instead of `div`

--- src/js/chat.html.js
+++ src/js/chat.html.js
@@ -1,6 +1,6 @@
 const template = document.createElement('template')
 template.innerHTML = /* html */ `
-  <div class="chat-messages"></div>
+  <section class="chat-messages"></section>
   <div class="chat-controls">
     <input type="text" name="message" id="message" placeholder="Message">
     <button class="submit">Send</button>
