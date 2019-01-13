78321bbc1dd2f054a0e9c9a8907c21b982e1390b
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-01-13 22:35:23 +0100

    Add default configuration

---
+++ src/js/config.js
@@ -0,0 +1,27 @@
+/**
+ * Configuration including very secret API keys, obiously not for production! ;)
+ */
+const configs = {
+  chatHost: 'ws://vhost3.lnu.se:20080/socket/',
+  apiKey: 'eDBE76deU7L0H9mEBgxUKVR0VCnq0XBd',
+  defaultChatChannel: 'my, not so secret, channel',
+  defaultUsername: 'Dude',
+  maxChatMessages: 100,
+  numBackgroundImages: 12,
+  animationDuration: 500,
+  availableApps: [
+    'memory',
+    'chat',
+    'notes',
+    'settings'
+  ],
+  availableThemes: [
+    'light',
+    'dark',
+    'minimal',
+    'weird'
+  ],
+  defaultTheme: 'light'
+}
+
+export default configs
