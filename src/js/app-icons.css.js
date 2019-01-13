f65de77b17ef0b6b39007677a4be1ee97abb2a09
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-01-13 22:37:44 +0100

    Update text/icon layout, add theme styling

--- src/js/app-icons.css.js
+++ src/js/app-icons.css.js
@@ -17,14 +17,59 @@ template.innerHTML = /* html */ `
       float: left;
       list-style: none;
       color: #fff;
-      padding: 1.2em;
       border-right: 1px solid #eee;
     }
 
     :host a {
+      padding: 1.2em;
       color: #fff;
+      text-decoration: none;
+      display: flex;
+      border-right: 1px solid rgba(200, 200, 200, 0.4);
+      transition: background 400ms, color 400ms
     }
-  </style>
+
+    :host a span {
+      display: flex;
+      align-items: center;
+      padding: .4em;
+    }
+
+    :host img {
+      filter: invert(100%);
+      padding-right: .4em
+    }
+
+    /**
+     * Light theme
+     */
+
+    :host-context(.light) {
+      background: #eee;
+    }
+
+    :host-context(.light) a {
+      color: #444;
+    }
+
+    :host-context(.light) a:hover {
+      color: #666;
+      background: #fff;
+    }
+
+    :host-context(.light) img {
+      filter: invert(10%);
+    }
+
+    /**
+     * Minimal theme
+     */
+
+    :host-context(.minimal) span {
+      display: none;
+    }
+
+</style>
 `
 
 export default template
