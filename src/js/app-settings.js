9c6dacbd2819a0b856e4551656070eb1b981475e
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-01-13 22:31:47 +0100

    Add support for theme switching, change background image config to not depend on defining filenames

--- src/js/app-settings.js
+++ src/js/app-settings.js
@@ -1,6 +1,7 @@
 import AppWindow from './appwindow.js'
 import Storage from './storage.js'
 import htmlTemplate from './app-settings.html.js'
+import cssTemplate from './app-settings.css.js'
 import Config from './config.js'
 import * as _ from './helpers.js'
 const storage = new Storage()
@@ -10,29 +11,52 @@ export default class SettingsApp extends AppWindow {
     super()
     const _this = this
 
+    this.imgPath = '/image/backgrounds/'
+
+    this._contentElem.appendChild(cssTemplate.content.cloneNode(true))
     this._contentElem.appendChild(htmlTemplate.content.cloneNode(true))
+
     this._imagesElem = this.shadowRoot.querySelector('.images')
+    this._themesList = this.shadowRoot.querySelector('#themes')
+
     this.setTitle('Settings')
-    this.imgList = Config.backgroundImages
 
     // setup images
     let htmlTemp = ''
-    for (let img of this.imgList) { htmlTemp += '<img src="' + img + '" width="50">' }
+
+    for (let i = 0; i < Config.numBackgroundImages; ++i) {
+      htmlTemp += '<img src="' + this.imgPath + i + '.jpg' + '">'
+    }
+
     _.addTemplate(htmlTemp, this._imagesElem)
 
     // setup click events for each imag
-    var elements = this._contentElem.getElementsByTagName('img')
-    for (var i = 0, len = elements.length; i < len; i++) {
+    let elements = this._contentElem.getElementsByTagName('img')
+    for (let i = 0, len = elements.length; i < len; i++) {
       elements[i].onclick = function () {
         _this.setBackgroundImage(this.src, document.body)
       }
     }
+
+    // setup themes
+    Config.availableThemes.forEach((theme) => {
+      let themeElem = document.createElement('p')
+      themeElem.textContent = theme
+      themeElem.addEventListener('click', (event) => {
+        this.setTheme(event.target.textContent)
+      })
+      this._themesList.appendChild(themeElem)
+    })
   }
 
   // TODO: remove duplication
   setBackgroundImage (img) {
     _.setBackgroundImage(img, document.body)
     storage.set('settings-background', img)
+  }
+
+  setTheme (theme) {
+    document.body.className = theme
   }
 }
 
