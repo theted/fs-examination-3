8052e7c1ff90329934e60d5cee502675c433d3a2
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-01-15 04:31:01 +0100

    Basic styling for notes list

--- src/js/notes.css.js
+++ src/js/notes.css.js
@@ -2,6 +2,20 @@ const template = document.createElement('template')
 template.innerHTML = /* html */ `
 
   <style>
+
+    /**
+     * Hard reset mostly for dem pesky lists
+     */
+    :host * {
+      padding: 0;
+      margin: 0;
+    }
+
+
+    /**
+     * Note edit
+     */
+
     :host textarea {
       width: calc(100% - 2em - 2px);
       min-height: 140px;
@@ -9,15 +23,39 @@ template.innerHTML = /* html */ `
       border: 1px solid #d0d0d0;
     }
 
-    :host button {
-      margin: 1em;
-      float: right;
-    }
-
     :host span.state {
       margin: 1em;
       float: left
     }
+
+
+    /**
+     * Notes list
+     */
+
+    :host #notes-list {
+      padding: 0;
+    }
+
+    :host #notes-list li {
+      list-style: none;
+    }
+
+    :host #notes-list a {
+      padding: 1em;
+      display: block;
+      border-bottom: 1px solid rgba(0,0,0,0.15);
+    }
+
+
+    /**
+     * Helper classes
+     */
+
+    :host .hidden {
+      display: none;
+    }
+
   </style>
 
 `
