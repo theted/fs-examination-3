0541a2f3f553e10e46e8be47c35731815d3e3301
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-01-15 05:09:36 +0100

    Set theme on application init

--- src/js/app-wrap.js
+++ src/js/app-wrap.js
@@ -6,6 +6,7 @@
 import './app-icons.js'
 import * as _ from './helpers.js'
 import Storage from './storage.js'
+import Config from './config.js'
 const storage = new Storage()
 
 export default class AppWrap extends window.HTMLElement {
@@ -18,6 +19,19 @@ export default class AppWrap extends window.HTMLElement {
 
     this.attachShadow({ mode: 'open' })
     this.shadowRoot.appendChild(template.content.cloneNode(true))
+
+    this.init()
+  }
+
+  /**
+   * Actions to perform when application starts
+   */
+  init () {
+    // set theme
+    let theme = storage.get('setting-theme') || Config.defaultTheme
+    document.body.className = theme
+
+    // set background image
     this.background = storage.get('settings-background')
     if (this.background) { _.setBackgroundImage(this.background, document.body) }
   }
